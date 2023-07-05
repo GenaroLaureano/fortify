89d28db2be2f1c9df9fcdfaebf40ef6a2f8a9f9f
GenaroLaureano <laureanomg2@gmail.com>
2023-07-05 08:03:33 -0600

    test

--- app/Http/Middleware/Keycloak.php
+++ app/Http/Middleware/Keycloak.php
@@ -11,36 +11,36 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Keycloak
 {
-    public function handle(Request $request, Closure $next)
-    {
-        $oidc = $this->getOpenIdConnectClient();
-        $oidc->providerConfigParam($this->getConfigParams());
-        $session = $oidc->introspectToken(session('user-access-token', ''));
+    // public function handle(Request $request, Closure $next)
+    // {
+    //     $oidc = $this->getOpenIdConnectClient();
+    //     $oidc->providerConfigParam($this->getConfigParams());
+    //     $session = $oidc->introspectToken(session('user-access-token', ''));
 
-        if (! $session->active) {
-            return redirect('/login');
-        }
+    //     if (! $session->active) {
+    //         return redirect('/login');
+    //     }
 
-        return $next($request);
-    }
+    //     return $next($request);
+    // }
 
-    private function getOpenIdConnectClient()
-    {
-        return new OpenIDConnectClient(
-            config('keycloak.authServerUrl'),
-            config('keycloak.realm'),
-            config('keycloak.clientSecret')
-        );
-    }
+    // private function getOpenIdConnectClient()
+    // {
+    //     return new OpenIDConnectClient(
+    //         config('keycloak.authServerUrl'),
+    //         config('keycloak.realm'),
+    //         config('keycloak.clientSecret')
+    //     );
+    // }
 
-    private function getConfigParams(): array
-    {
-        try {
-            $config_params = Http::get('http://0.0.0.0:8080/realms/hub-dev/.well-known/openid-configuration');
-        } catch (Exception $e) {
-            throw new HttpException(503, 'Service Unavailable', null);
-        }
+    // private function getConfigParams(): array
+    // {
+    //     try {
+    //         $config_params = Http::get('http://0.0.0.0:8080/realms/hub-dev/.well-known/openid-configuration');
+    //     } catch (Exception $e) {
+    //         throw new HttpException(503, 'Service Unavailable', null);
+    //     }
 
-        return $config_params->collect()->toArray();
-    }
+    //     return $config_params->collect()->toArray();
+    // }
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,7 +2,11 @@
 
 namespace Tests\Unit;
 
+use Exception;
 use PHPUnit\Framework\TestCase;
+use Illuminate\Support\Facades\Http;
+use Jumbojett\OpenIDConnectClient;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class ExampleTest extends TestCase
 {
@@ -13,6 +17,26 @@ class ExampleTest extends TestCase
      */
     public function test_that_true_is_true()
     {
-        $this->assertTrue(true);
+        $this->assertIsArray($this->getConfigParams())
+    }
+
+    private function getOpenIdConnectClient()
+    {
+        return new OpenIDConnectClient(
+            config('keycloak.authServerUrl'),
+            config('keycloak.realm'),
+            config('keycloak.clientSecret')
+        );
+    }
+
+    private function getConfigParams(): array
+    {
+        try {
+            $config_params = Http::get(config('keycloak.authServerUrl').'/realms/'.config('keycloak.realm').'/.well-known/openid-configuration');
+        } catch (Exception $e) {
+            throw new HttpException(503, $e->getMessage());
+        }
+
+        return $config_params->collect()->toArray();
     }
 }
