965a9dae5152d16b9c7ef19d3076686632d32329
GenaroLaureano <laureanomg2@gmail.com>
2023-07-05 17:48:07 -0600

    test

--- keycloak/entrypoint.sh
+++ keycloak/entrypoint.sh
@@ -5,6 +5,6 @@ exec docker run \
     -p $INPUT_KEYCLOAK_HTTP_PORT:8080 \
     -e KEYCLOAK_ADMIN=$INPUT_KEYCLOAK_ADMIN_USER \
     -e KEYCLOAK_ADMIN_PASSWORD=$INPUT_KEYCLOAK_ADMIN_PASS \
-    -v "/$(pwd)/keycloak/realms":"/opt/keycloak/data/import" \
+    -v "/$(pwd)/realms":"/opt/keycloak/data/import" \
     quay.io/keycloak/keycloak:$INPUT_KEYCLOAK_VERSION \
     start-dev --import-realm
